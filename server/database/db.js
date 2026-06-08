588859c0d20640672baeb553cd382620c8cd0424
Andrea Luna <andrea.lunaccn@hotmail.com>
2026-06-08 17:03:37 -0400

    fix netlify routes

--- server/database/db.js
+++ server/database/db.js
@@ -1,20 +1,30 @@
-const { Pool } = require('pg')
-require('dotenv').config()
+const { Pool } = require("pg");
+require("dotenv").config();
 
 const pool = new Pool({
-    connectionString: process.env.CONNECTION_STRING, 
-    allowExitOnIdle: true
-})
+  connectionString: process.env.CONNECTION_STRING,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
-const getHealth = async () =>{
-   try {
-    const result = await pool.query("SELECT NOW()")
-   console.log("🟢 Base de datos conectada y funcionando a las " + result.rows[0].now)
-   } catch (error) {
-    console.error("❌ Error conectando a la base de datos: "+ error.message)
-   }
-}
+const getHealth = async () => {
+  try {
+    const result = await pool.query("SELECT NOW()");
 
-getHealth()
+    console.log(
+      "✅ Base de datos conectada y funcionando a las " +
+        result.rows[0].now
+    );
 
-module.exports = pool
+  } catch (error) {
+    console.error(
+      "❌ Error conectando a la base de datos: " +
+        error.message
+    );
+  }
+};
+
+getHealth();
+
+module.exports = pool;
