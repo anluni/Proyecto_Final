46925ffc9309ac939da7fdd0891912caa2887659
Andrea Luna <andrea.lunaccn@hotmail.com>
2026-06-08 17:56:05 -0400

    actualización tablas

--- server/database/db.js
+++ server/database/db.js
@@ -27,4 +27,22 @@ const getHealth = async () => {
 
 getHealth();
 
+pool.query(`
+  CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    email TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    role TEXT
+  );
+
+  CREATE TABLE IF NOT EXISTS item (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    price INTEGER,
+    image TEXT
+  );
+`)
+.then(() => console.log("✅ Tablas creadas"))
+.catch(err => console.error(err));
+
 module.exports = pool;
